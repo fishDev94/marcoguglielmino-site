d2f29af3e32f2a55cc9332a5b7cf60aa9b977343
marco94gug <marco.guglielmino94@gmail.com>
2026-07-05 20:00:09 +0200

    feat: ui components

---
+++ app/components/UI/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <UInput
+    v-model="value"
+    icon="i-lucide-search"
+    size="md"
+    class="rounded-none"
+    variant="outline"
+    placeholder="Search..."
+  >
+    <template
+      v-if="value?.length"
+      #trailing
+    >
+      <UButton
+        color="neutral"
+        variant="link"
+        size="sm"
+        icon="i-lucide-circle-x"
+        aria-label="Clear input"
+        @click="onClear"
+      />
+    </template>
+  </UInput>
+</template>
+
+<script setup lang="ts">
+const value = ref('')
+
+const onClear = () => {
+  value.value = ''
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,23 +3,40 @@
     <h1 class="my-text">
       Test test
     </h1>
-    <UIButton type="primary">
-      Primary
-    </UIButton>
-    <UIButton type="secondary">
-      Secondary
-    </UIButton>
-    <UIButton type="inverted">
-      Inverted
-    </UIButton>
-    <UIButton type="outlined">
-      Outlined
-    </UIButton>
+    <div class="button-content">
+      <UIButton
+        type="primary"
+        is-strong
+      >
+        Primary
+      </UIButton>
+      <UIButton type="secondary">
+        Secondary
+      </UIButton>
+      <UIButton type="inverted">
+        Inverted
+      </UIButton>
+      <UIButton type="outlined">
+        Outlined
+      </UIButton>
+      <UIButton
+        type="outlined-light"
+        is-strong
+      >
+        Outlined Light
+      </UIButton>
+    </div>
+    <UISearchBar />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .my-text {
   @include heading(1);
+}
+
+.button-content {
+  display: flex;
+  gap: 4px;
 }
 </style>
