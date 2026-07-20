df448568b2cc21d77258feadfad969ea66edda30
MarcoGuglielmino <marco.guglielmino@musement.com>
2026-07-20 23:47:20 +0200

    feat: cleanup code

---
+++ app/components/Instagram/TopReels.vue
@@ -0,0 +1,27 @@
+<template>
+  <TopSection
+    section-name="instagram"
+    background="secondary-dark"
+    carousel-item-size="320px"
+  >
+    <template v-if="isReelsDataLoading">
+      <InstagramReelCardSkeleton
+        v-for="n in REEL_CARD_COUNT"
+        :key="`${n}+reel-skeleton`"
+      />
+    </template>
+    <template v-else>
+      <InstagramReelCard
+        v-for="reel in reels"
+        :key="`${reel.id}+instagram-reel-card`"
+        :reel
+      />
+    </template>
+  </TopSection>
+</template>
+
+<script lang="ts" setup>
+import { REEL_CARD_COUNT } from "~/constants"
+
+const { reels, isReelsDataLoading } = useInstagramReelData(ref(REEL_CARD_COUNT))
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,67 +13,19 @@
       :key="contentBlock?.slug || `${idx}+content-block-home`"
       :content-data="contentBlock"
     />
-    <TopSection
-      section-name="strava"
-      link-to="strava"
-      background="default-white"
-    >
-      <template v-if="isLoadingStravaData">
-        <StravaActivityCardSkeleton
-          v-for="n in LAST_ACTIVITIES_PER_PAGE"
-          :key="`${n}+strava-skeleton`"
-        />
-      </template>
-      <template v-else>
-        <StravaActivityCard
-          v-for="(activity, i) in activities"
-          :key="`activity-card-${activity.id}+${i}`"
-          :activity
-        />
-      </template>
-    </TopSection>
-    <TopSection
-      section-name="instagram"
-      background="secondary-dark"
-      carousel-item-size="320px"
-    >
-      <template v-if="isReelsDataLoading">
-        <InstagramReelCardSkeleton
-          v-for="n in REEL_CARD_COUNT"
-          :key="`${n}+reel-skeleton`"
-        />
-      </template>
-      <template v-else>
-        <InstagramReelCard
-          v-for="reel in reels"
-          :key="`${reel.id}+instagram-reel-card`"
-          :reel
-        />
-      </template>
-    </TopSection>
+    <StravaLastActivities />
+    <InstagramTopReels />
   </div>
 </template>
 
 <script setup lang="ts">
-import { LAST_ACTIVITIES_PER_PAGE, REEL_CARD_COUNT } from "~/constants"
-import InstagramReelCardSkeleton from "~/components/Instagram/ReelCardSkeleton.vue"
-import StravaActivityCardSkeleton from "~/components/Strava/ActivityCardSkeleton.vue"
-
-import type { CtaButtonDataFragment } from "#gql"
-
 definePageMeta({
   name: "home"
 })
 
-const { reels, isReelsDataLoading } = useInstagramReelData(ref(REEL_CARD_COUNT))
-const { getActivities } = useStravaActivities()
-
-const { data: activities, pending: isLoadingStravaData } = getActivities({ per_page: LAST_ACTIVITIES_PER_PAGE })
-
 const {
   homepageData,
-  contentBlocks
+  contentBlocks,
+  ctaButtons
 } = await useAsyncHomepageData()
-
-const ctaButtons = computed(() => homepageData.value.ctaButton?.items as CtaButtonDataFragment[])
 </script>
