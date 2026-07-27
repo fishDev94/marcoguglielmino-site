e0b66be4daa2357121ae35d45627ccd8e950e403
MarcoGuglielmino <marco.guglielmino@musement.com>
2026-07-27 16:39:04 +0200

    feat: put links on blog page for testing

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -5,14 +5,18 @@
       :ui="{ link: 'text-xs md:text-sm' }"
     />
     <h1>blog</h1>
-    <NuxtLinkLocale
-      :to="{ name: 'blog-post-page',
-             params: {
-               slug: '10k-valencia-2026'
-             } }"
-    >
-      test to valencia
-    </NuxtLinkLocale>
+    <div style="display: flex; flex-direction: column;">
+      <NuxtLinkLocale
+        v-for="(article, i) in articles"
+        :key="`${article?.slug}+${i}`"
+        :to="{ name: 'blog-post-page',
+               params: {
+                 slug: article?.slug
+               } }"
+      >
+        {{ article?.title }}
+      </NuxtLinkLocale>
+    </div>
   </div>
 </template>
 
